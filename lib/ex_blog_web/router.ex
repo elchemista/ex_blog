d6b5afe3695dbe9db503262a2ed792455e35c005
Yuriy Zhar <darkyfelis@gmail.com>
2026-08-04 00:04:32 +0200

    Protect the admin routes

--- lib/ex_blog_web/router.ex
+++ lib/ex_blog_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ExBlogWeb.Router do
   use ExBlogWeb, :router
 
+  import ExBlogWeb.AdminAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -18,6 +20,19 @@ defmodule ExBlogWeb.Router do
     plug ExBlogWeb.Plugs.MCPSecurity
   end
 
+  pipeline :admin_browser do
+    plug :put_security_headers
+    plug :fetch_current_scope
+  end
+
+  pipeline :redirect_if_admin_authenticated do
+    plug :redirect_if_authenticated
+  end
+
+  pipeline :require_admin_authenticated do
+    plug :require_authenticated
+  end
+
   scope "/", ExBlogWeb do
     pipe_through :browser
 
@@ -29,6 +44,32 @@ defmodule ExBlogWeb.Router do
     get "/robots.txt", FeedController, :robots
     get "/feed.xml", FeedController, :rss
     get "/atom.xml", FeedController, :atom
+  end
+
+  scope "/admin", ExBlogWeb.Admin do
+    pipe_through [:browser, :admin_browser, :redirect_if_admin_authenticated]
+
+    live_session :redirect_if_admin_authenticated,
+      on_mount: [{ExBlogWeb.AdminAuth, :redirect_if_authenticated}] do
+      live "/login", LoginLive, :new
+    end
+
+    post "/login", SessionController, :create
+  end
+
+  scope "/admin", ExBlogWeb.Admin do
+    pipe_through [:browser, :admin_browser]
+
+    delete "/logout", SessionController, :delete
+  end
+
+  scope "/admin", ExBlogWeb.Admin do
+    pipe_through [:browser, :admin_browser, :require_admin_authenticated]
+
+    live_session :require_admin_authenticated,
+      on_mount: [{ExBlogWeb.AdminAuth, :require_authenticated}] do
+      live "/telegram", TelegramLive, :show
+    end
   end
 
   scope "/", ExBlogWeb do
