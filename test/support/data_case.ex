da13831a9e466a0c596241054d7518ade08b0bff
Yuriy Zhar <darkyfelis@gmail.com>
2026-08-04 01:18:18 +0200

    test: reset durable storage between tests

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -1,31 +1,20 @@
 defmodule ExBlog.DataCase do
   @moduledoc """
-  Test case for code that talks to the ExBlog SQLite repository.
+  Test case for code that uses ExBlog's durable runtime storage.
   """
 
   use ExUnit.CaseTemplate
 
-  alias Ecto.Adapters.SQL.Sandbox
-  alias ExBlog.Repo
+  alias ExBlog.Storage
 
   using do
     quote do
-      alias ExBlog.Repo
-
-      import Ecto
-      import Ecto.Changeset
-      import Ecto.Query
       import ExBlog.DataCase
     end
   end
 
-  setup tags do
-    ExBlog.DataCase.setup_sandbox(tags)
+  setup _tags do
+    :ok = Storage.clear()
     :ok
-  end
-
-  def setup_sandbox(tags) do
-    owner = Sandbox.start_owner!(Repo, shared: not tags[:async])
-    on_exit(fn -> Sandbox.stop_owner(owner) end)
   end
 end
