339da7d4d5957296cca39cb1dc98519d0cc11956
Yuriy Zhar <darkyfelis@gmail.com>
2026-08-04 01:18:18 +0200

    test: remove SQL sandbox from connection tests

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -3,22 +3,11 @@ defmodule ExBlogWeb.ConnCase do
   This module defines the test case to be used by
   tests that require setting up a connection.
 
-  Such tests rely on `Phoenix.ConnTest` and also
-  import other functionality to make it easier
-  to build common data structures and query the data layer.
-
-  Finally, if the test case interacts with the database,
-  we enable the SQL sandbox, so changes done to the database
-  are reverted at the end of every test. If you are using
-  PostgreSQL, you can even run database tests asynchronously
-  by setting `use ExBlogWeb.ConnCase, async: true`, although
-  this option is not recommended for other databases.
+  Such tests rely on `Phoenix.ConnTest` and import the conveniences needed to
+  exercise the HTTP boundary. ExBlog has no SQL repository.
   """
 
   use ExUnit.CaseTemplate
-
-  alias Ecto.Adapters.SQL.Sandbox
-  alias ExBlog.Repo
 
   using do
     quote do
@@ -34,10 +23,7 @@ defmodule ExBlogWeb.ConnCase do
     end
   end
 
-  setup tags do
-    owner = Sandbox.start_owner!(Repo, shared: not tags[:async])
-    on_exit(fn -> Sandbox.stop_owner(owner) end)
-
+  setup _tags do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
