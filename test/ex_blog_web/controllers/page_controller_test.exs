4d00f54e1230bbe7578e7e7fa5c2479ce814a9e9
Yuriy Zhar <darkyfelis@gmail.com>
2026-08-03 23:13:03 +0200

    Update test/ex_blog_web/controllers/page_controller_test.exs

--- test/ex_blog_web/controllers/page_controller_test.exs
+++ test/ex_blog_web/controllers/page_controller_test.exs
@@ -1,8 +1,14 @@
-defmodule ExBlogWeb.PageControllerTest do
+defmodule ExBlogWeb.PublicHomeTest do
   use ExBlogWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  test "GET / renders the blog shell without indexed content", %{conn: conn} do
+    document =
+      conn
+      |> get(~p"/")
+      |> html_response(200)
+      |> LazyHTML.from_document()
+
+    assert document |> LazyHTML.query("#blog-index") |> Enum.count() == 1
+    assert document |> LazyHTML.query("#articles-empty") |> Enum.count() == 1
   end
 end
